a114256e64954c3532146779ef48776be7f71990
carlosmatheus06 <carlos.rodrigues131@etec.sp.gov.br>
2022-11-25 12:34:35 +0000

    Preparação da Base de Dados

---
+++ assets/sql/create_database.sql
@@ -0,0 +1,15 @@
+create database biblioteca;
+
+use biblioteca;
+
+create table generos(
+    id int auto_increment primary key,
+    nome varchar(255) not null
+);
+
+create table livros(
+    id int auto_increment primary key,
+    título varchar(255) not null,
+    id_genero int not null,
+    foreign key (id_genero) references generos(id)
+);
---
+++ assets/sql/dados_iniciais.sql
@@ -0,0 +1,11 @@
+insert into generos(id, nome) values
+(1, 'Alta Fantasia'),
+(2, 'Policial'),
+(3, 'Drama'),
+(4, 'Ficção Científica');
+
+insert into livros(id, título, id_genero) values
+(1, 'Assassinato no Expresso do Oriente', 2),
+(2, 'A Sociedade do Anel', 1),
+(3, 'Como eu era Antes de Você', 3),
+(4, 'O Guia do Mochileiro das Galáxias', 4);
